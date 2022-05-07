45863359941c3fe5719e3d003052bed79e077432
P1kaj1uu <891523233@qq.com>
2022-05-07 17:25:31 +0800

    优化了注册页面功能

--- js/countdown.js
+++ js/countdown.js
@@ -1,13 +1,14 @@
 // 获取手机号输入框
 let telInp = document.querySelector('#tel')
 // 手机号的正则表达式
-let TelReg = /^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
+let TelReg =
+  /^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
 
 // 验证码倒计时
 let djs = 60
 // 获取验证码按钮
-let btn = document.querySelector(".getBtn")
-btn.addEventListener("click", function () {
+let btn = document.querySelector('.getBtn')
+btn.addEventListener('click', function () {
   // 当手机号码格式正确时
   if (TelReg.test(telInp.value)) {
     btn.style.disabled = true
@@ -17,7 +18,7 @@ btn.addEventListener("click", function () {
       if (djs === 0) {
         clearInterval(timer1)
         btn.style.disabled = false
-        btn.innerHTML = `获取验证码`
+        btn.innerHTML = `重新获取验证码`
         djs = 60
       }
     }, 1000)
